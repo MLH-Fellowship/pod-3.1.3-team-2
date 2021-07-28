a4efe5b9daa71f53cb364244dd6cd2cf0d3d6df7
MsMore <more.riddhi11@gmail.com>
2021-07-28 18:38:30 +0530

    added temporary save and search

--- popup.js
+++ popup.js
@@ -1,24 +1,25 @@
-// Initialize butotn with users's prefered color
-let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be execuetd as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
+function gettext() {
+  var ele = document.getElementById("textarea").value;
+  var lists = document.getElementById('list');
+  if(ele!="")
+    lists.innerHTML += "<li>" + ele + "</li>";
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('link');
+  // onClick's logic below:
+  link.addEventListener('click', function() {
+
+      //there is no need to declare a new function here.
+
+      var one = document.getElementById("textarea").value;
+      var two = 'https://www.google.com/search?q=' + encodeURIComponent(one);
+      if (one!="")
+        window.open(two);
+  });
+});
+
+document.getElementById("save").addEventListener('click', function (event) { 
+  gettext();// Add code - for you call function 
+});
