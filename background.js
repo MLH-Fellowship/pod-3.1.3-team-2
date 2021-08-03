fd08f5a2fd6c5acbd4b3aae654204c8f977091f5
Lucas Vinzon <lucasvinzon@gmail.com>
2021-08-03 16:39:38 -0300

    probable solution

--- background.js
+++ background.js
@@ -6,12 +6,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     console.log(sender.tab ?
         "from a content script:" + sender.tab.url :
         "from the extension");
-
-        
-
-if (request.greeting === "hello")
-sendResponse({farewell: "goodbye"});
-
+        addItemToList(request.greeting);
 })
 
 //working on highlight
